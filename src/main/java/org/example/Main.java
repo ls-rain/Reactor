88317b9f5827224d1cafae77eb57a21d97f282e7
ls-rain <swlee19990115@gmail.com>
2024-05-22 13:36:50 +0900

    Flux/Mono 연습

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Publisher publisher = new Publisher();
+//        publisher.startFlux()
+//                        .subscribe(System.out::println);
+
+        publisher.startMono2()
+                        .subscribe();
         System.out.println("Hello world!");
     }
+
 }
